8b6892ff897a6e03b045535393877bca0f7fd57c
Tekling <32271622+Tekling@users.noreply.github.com>
2017-11-05 12:22:09 -0500

    Started creating Domain objects

---
+++ GhostGroove/src/main/java/com/revature/domain/Comment.java
@@ -0,0 +1,54 @@
+package com.revature.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity 
+@Table(name="COMMENT")
+public class Comment {
+	
+	public Comment() {
+		super();
+	}
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="COMMENT_SEQ")
+	@SequenceGenerator(allocationSize=1,name="CommentSequence",sequenceName="SQ_COMMENT_PK")
+	@Column(name="C_ID")
+	//@OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.ALL)
+	//@JoinColumn(name="P_ID")
+	private int id;
+	
+	@Column(name="P_ID")
+	private int playlistId;
+	
+	@Column(name="P_CONTENT")
+	private String content;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getPlaylistId() {
+		return playlistId;
+	}
+	public void setPlaylistId(int playlistId) {
+		this.playlistId = playlistId;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
