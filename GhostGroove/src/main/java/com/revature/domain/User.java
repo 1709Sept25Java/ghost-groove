9a8509f82530ebd16a846404e14c9825d6527138
SabinaHaque <sabinaahaque@gmail.com>
2017-11-05 23:07:50 -0500

    Just added the Daos

--- GhostGroove/src/main/java/com/revature/domain/User.java
+++ GhostGroove/src/main/java/com/revature/domain/User.java
@@ -1,5 +1,101 @@
 package com.revature.domain;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity 
+@Table(name="USER")
 public class User {
+
+	public User() {
+		super();
+	}
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="USER_SEQ")
+	@SequenceGenerator(allocationSize=1,name="UserSequence",sequenceName="SQ_USER_PK")
+	@Column(name="U_ID")
+	@ManyToMany(fetch=FetchType.EAGER, cascade=CascadeType.ALL)
+	@JoinColumn(name="P_ID")
+	private int id;
+	
+	@Column(name="USERNAME")
+	@NotNull
+	private String Username;
+	
+	@Column(name="PASSWORD")
+	@NotNull
+	private String Password;
+	
+	@Column(name="FIRSTNAME")
+	private String Firstname;
+	
+	@Column(name="LASTNAME")
+	private String Lastname;
+	
+	@Column(name="EMAIL")
+	private String Email;
+	
+	@Column(name="ISMANAGER")
+	@NotNull
+	private String isManager;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}	
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getFirstname() {
+		return Firstname;
+	}
+	public void setFirstname(String firstname) {
+		Firstname = firstname;
+	}
+	public String getLastname() {
+		return Lastname;
+	}
+	public void setLastname(String lastname) {
+		Lastname = lastname;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getIsManager() {
+		return isManager;
+	}
+	public void setIsManager(String isManager) {
+		this.isManager = isManager;
+	}
 
 }
