72fd6627c48b23f6b9d5ade92ded7495dd4953ec
rcocode <rebeca_otero@live.com>
2017-11-13 19:26:32 -0500

    Fixing Ajax calls

--- GhostGroove/src/main/webapp/WEB-INF/static/script/home.js
+++ GhostGroove/src/main/webapp/WEB-INF/static/script/home.js
@@ -1,14 +1,14 @@
 var app = angular.module('homeApp',[]);
 
 app.controller('genreCtrl',function($scope,$http){
-	var url = "http://localhost:8080/GhostGroove/genre/all";
+	var url = "http://ec2-52-14-255-113.us-east-2.compute.amazonaws.com:8080/GhostGroove/genre/all";
 	$http.get(url).then(function(response){
 		$scope.genres = response.data
 	})
 })
 
 app.controller('playlistCtrl',function($scope,$http){
-	var url = "http://localhost:8082/GhostGroove/playlist/byuser";
+	var url = "http://ec2-52-14-255-113.us-east-2.compute.amazonaws.com:8080/GhostGroove/playlist/byuser";
 	$http.get(url).then(function(response){
 		$scope.playlists = response.data
 		console.log($scope.playlists)
@@ -17,7 +17,7 @@ app.controller('playlistCtrl',function($scope,$http){
 	$scope.playlist={id:0,name:null,description:null,genre:null,comments:null,owners:null,songs:null};
 	$scope.newPlaylist = function(){
 		$scope.playlist.genre = JSON.parse($scope.playlist.genre)
-		var url="http://localhost:8082/GhostGroove/playlist/add";
+		var url="http://ec2-52-14-255-113.us-east-2.compute.amazonaws.com:8080/GhostGroove/playlist/add";
 		console.log(JSON.stringify($scope.playlist));
 		$http.post(url,JSON.stringify($scope.playlist)).then(function(response){
 			$scope.status = "Success!";
--- GhostGroove/src/main/webapp/WEB-INF/static/script/viewUsers.js
+++ GhostGroove/src/main/webapp/WEB-INF/static/script/viewUsers.js
@@ -2,7 +2,7 @@
 var app = angular.module('allUsersApp',[]);
 
 app.controller('usersCtrl',function($scope,$http){
-	var url="http://localhost:8080/GhostGroove/user/allUsers";
+	var url="http://ec2-52-14-255-113.us-east-2.compute.amazonaws.com:8080/GhostGroove/user/allUsers";
 	$http.get(url).then(function(response){
 		console.log(response.data);
 		$scope.users = response.data;
